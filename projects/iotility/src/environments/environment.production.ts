0a88d098fb62570944c7d60cc6157f48297dc535
Haris Khan <haris.k@hypernymbiz.com>
2026-07-16 14:28:49 +0500

    fix

--- projects/iotility/src/environments/environment.production.ts
+++ projects/iotility/src/environments/environment.production.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   apiBaseUrl: 'https://staging.gateway.iot.vodafone.com.qa',
   userMsBaseUrl: 'https://at.admin.staging.iot.vodafone.com.qa',
-  fleetBaseUrl: 'https://prodgateway.hypernymbiz.com/fms-fleet',
+  fleetBaseUrl: 'https://staging.gateway.iot.vodafone.com.qa/fmsfleet',
   signalRUrl: 'https://func-stag-all.azurewebsites.net',
   useCaseId: 6,
 } as const;
