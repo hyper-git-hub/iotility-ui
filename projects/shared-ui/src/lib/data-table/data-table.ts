b5bf793a33c086405bcc02a7065b473ee98753b1
Haris Khan <haris.k@hypernymbiz.com>
2026-07-24 19:20:23 +0500

    Routes page modified

--- projects/shared-ui/src/lib/data-table/data-table.ts
+++ projects/shared-ui/src/lib/data-table/data-table.ts
@@ -12,7 +12,7 @@ export interface TableColumn {
   clickable?: boolean;
 }
 export type TableRow = Record<string, string | number | boolean>;
-export type TableAction = 'view' | 'map' | 'history' | 'edit' | 'delete';
+export type TableAction = 'view' | 'map' | 'history' | 'edit' | 'delete' | 'dispatch';
 export interface ExpandedRowContext {
   $implicit: TableRow;
   row: TableRow;
