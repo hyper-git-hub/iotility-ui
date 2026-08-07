5f353587d3addc8e71b4869f3fa3c9ff6567f262
Hamza Ali <hamza.ali.art3@gmail.com>
2026-08-07 13:33:13 +0500

    logo import

--- projects/shared-ui/src/public-api.ts
+++ projects/shared-ui/src/public-api.ts
@@ -8,3 +8,4 @@ export * from './lib/skeleton/skeleton';
 export * from './lib/skeleton/stat-card-skeleton';
 export * from './lib/skeleton/data-table-skeleton';
 export * from './lib/tooltip/tooltip';
+export * from './lib/logo/logo';
